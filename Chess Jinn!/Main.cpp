2aa909b9575d8709b7eb2f52c722a7cc2834a640
vvacenov <102173582+vvacenov@users.noreply.github.com>
2023-08-23 23:34:31 +0200

    Update Main.cpp

--- Chess Jinn!/Main.cpp
+++ Chess Jinn!/Main.cpp
@@ -1,5 +1,7 @@
 #include "RenderController.h"
 
+// a work in progress, while trying out the SFML library. Hopefully, I will give this project regular updates
+
 int main(void)
 {
 	RenderController d;
